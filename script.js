a1b847411ec9bde45d856756cb6ab8c5d45d4b6b
Nikhil Khemani <khemaninikhil2@gmail.com>
2024-07-07 19:12:29 +0530

    Updated CV

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ function loco() {
       locoScroll.scrollTo("#page8")
     })
     
-    document.querySelector("#page9").addEventListener("click", () => {
+    document.querySelector("#page10").addEventListener("click", () => {
       locoScroll.scrollTo("#page8")
     })
   
@@ -308,3 +308,95 @@ loaderAnimation();
 
 
 
+
+
+
+
+
+
+
+
+// // Page 9
+
+// $(function () {
+
+//   $('#containerr').highcharts({
+
+//     chart: {
+//       polar: true,
+//       type: 'line'
+//     },
+//     credits: {
+//       enabled: false
+//     },
+//     title: {
+//       text: 'Skill Breakdown'
+//     },
+
+//     pane: {
+//       startAngle: 0,
+//       endAngle: 360
+//     },
+
+//     xAxis: {
+//       type: 'category',
+//       tickInterval: 1,
+//       categories: ['Python', 'SQL', 'HTML, CSS, JS', 'R', 'JAVA', 'PHP', 'Dot.Net', 'C#'],
+//       min: 0,
+//       max: 8,
+//       tickmarkPlacement: 'on',
+
+//       lineWidth: 0,
+//       labels: {
+//         formatter: function () {
+//           return this.value
+//         }
+//       },
+
+//     },
+//     tooltip: {
+//       shared: true,
+//       useHTML: true,
+//       headerFormat: '<div class="newTip"><big>{point.key}</big>' + '<br/>',
+//       pointFormat: '{point.y} / 5.0',
+//       footerFormat: '</div>',
+//       valueDecimals: 1
+//     },
+//     yAxis: {
+//       gridLineInterpolation: 'polygon',
+//       min: 0,
+//       max: 5,
+//       tickInterval: 1,
+//       minorTickInterval: 0.5,
+//       showLastLabel: 'true',
+//       labels: {
+//         x: 8,
+//         style: {
+//           color: '#000',
+//           textShadow: '1px 1px 0px #fff',
+//           display: "inline-block"
+//         }
+//       },
+
+//     },
+
+//     plotOptions: {
+//       series: {
+//         pointStart: 0,
+//         pointInterval: 1,
+
+//       },
+//       column: {
+//         pointPadding: 0,
+//         groupPadding: 0
+//       }
+//     },
+
+//     series: [{
+//       type: 'area',
+//       name: 'Skills',
+//       data: [5, 4.5, 3.4, 4, 4.5, 4.1, 4.3, 4],
+//       pointPlacement: "on"
+//     }]
+//   });
+// });
